c6959fe061cb330c201ef05f9088d9703627670a
Bastián Garcés <bmgarcesdiaz@gmail.com>
2017-05-24 13:44:51 -0400

    .

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php include('header.php')?>
+
+<div class="row">
+<div class="col-sm-12">
+<h3>Sobre el proyecto</h3>
+<p>Mi proyecto se va a basar en la visualización de datos obtenidos a través de Transparencia de los gastos que realizan los hospitales más importantes de Santiago respecto a la compra directa de medicamentos.</p>
+<p>En Chile más de <strong>13 millones</strong> de personas se atienden  en el Sistema Público de Salud chileno. Lo que corresponde al <strong>71%</strong> de los chilenos.</p>
+<p>La deuda hospitalaria es de más de $255 mil millones y crece anualmente en promedio un <strong>18%</strong>. El <strong>98%</strong> de esta deuda se debe a la compra de insumos médicos.</p>
+<p>En base a estos antecedentes es que junto a un compañero de mi carrera estamos trabajando en un reportaje de investigación para descrubir porque sucede la compra directa de medicamentos, qué efectos tiene para la población que se atiende en estos recientes y quiénes son los beneficiados por esta práctica.</p>
+<p>La idea de esta página es poder alojar el reportaje que estamos elaborando para nuestro ramo de Taller de Periodismo Avanzado, pero también poder visualizar los datos que hemos obtenidos y que sea más fácil de entender para los lectores, adicionalmente, la idea es poder ampliar nuestro reportaje y que  multimedia, ya que obtendremos mucha información que se quedará fuera de nuestro reportaje final.</p>
+
+</div><!--/col-sm-12-->
+</div><!--/row->
+
+<?php include('footer.php')?>
---
+++ footer.php
@@ -0,0 +1,10 @@
+</div><!--/container-->
+<footer class="footer">
+<div class="container">
+<p class="text-muted"><?php echo $title;?> por Bastián Garcés. 24 de mayo, 2017. Diseño y Nuevos Medios.</p>
+</div>
+</footer>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
